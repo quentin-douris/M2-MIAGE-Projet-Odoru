9bfccf5c5f4f11980b2b40a5aee114c55e96907d
Quentin <quentin.douris63@gmail.com>
2022-03-21 16:04:35 +0100

    API Gateway Service

---
+++ Odoru-APIGatway/src/test/java/com/miage/odoru/projet/odoruapigatway/OdoruApiGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.odoru.projet.odoruapigatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoruApiGatwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
