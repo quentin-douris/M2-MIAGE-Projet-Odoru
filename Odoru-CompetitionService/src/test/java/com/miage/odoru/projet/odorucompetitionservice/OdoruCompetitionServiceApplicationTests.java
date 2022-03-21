fad19b07fdcaa0434483cb527b8f7ceedbd3349d
Quentin <quentin.douris63@gmail.com>
2022-03-21 16:53:07 +0100

    Competition Service

---
+++ Odoru-CompetitionService/src/test/java/com/miage/odoru/projet/odorucompetitionservice/OdoruCompetitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.odoru.projet.odorucompetitionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoruCompetitionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
