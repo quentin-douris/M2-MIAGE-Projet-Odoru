ac0b26aa429dfb98523f9248159443d41382f768
Quentin <quentin.douris63@gmail.com>
2022-03-21 17:04:52 +0100

    Statistique Service

---
+++ Odoru-StatistiqueService/src/test/java/com/miage/odoru/projet/odorustatistiqueservice/OdoruStatistiqueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.odoru.projet.odorustatistiqueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoruStatistiqueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
